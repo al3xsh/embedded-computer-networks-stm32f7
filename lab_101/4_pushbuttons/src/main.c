857ff2e548f08c4597da49c593ad463761eeeb70
Alex Shenfield <a.shenfield@shu.ac.uk>
2017-10-19 18:54:18 +0100

    tried to make sure the pins in the exercises map to those in the lab sheets

--- lab_101/4_pushbuttons/src/main.c
+++ lab_101/4_pushbuttons/src/main.c
@@ -18,10 +18,10 @@
 #include "clock.h"
 #include "gpio.h"
 
-// map the led to GPIO PI_1 (the inbuilt led) and the push button to PA_* 
-// (the user supplied button)
+// map the led to GPIO PI_1 (the inbuilt led) and the push button to PI_11 
+// (the user button)
 gpio_pin_t led = {PI_1, GPIOI, GPIO_PIN_1};
-gpio_pin_t pb1 = {PA_8, GPIOA, GPIO_PIN_8};
+gpio_pin_t pb1 = {PI_11, GPIOI, GPIO_PIN_11};
 
 // this is the main method
 int main()
