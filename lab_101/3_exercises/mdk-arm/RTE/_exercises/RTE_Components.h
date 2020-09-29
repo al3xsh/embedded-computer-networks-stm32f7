8cafe6c100fd7e4a53bb122532d0b1ea0a5f3df1
Alex Shenfield <a.shenfield@shu.ac.uk>
2020-09-29 18:27:51 +0100

    updated code for lab 101 - task 3 to use the new pack structure

---
+++ lab_101/3_exercises/mdk-arm/RTE/_exercises/RTE_Components.h
@@ -0,0 +1,37 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'exercises' 
+ * Target:  'exercises' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f7xx.h"
+
+/*  Keil::Device:STM32Cube Framework:Classic:1.2.7 */
+#define RTE_DEVICE_FRAMEWORK_CLASSIC
+/*  Keil::Device:STM32Cube HAL:Common:1.2.7 */
+#define RTE_DEVICE_HAL_COMMON
+/*  Keil::Device:STM32Cube HAL:Cortex:1.2.7 */
+#define RTE_DEVICE_HAL_CORTEX
+/*  Keil::Device:STM32Cube HAL:GPIO:1.2.7 */
+#define RTE_DEVICE_HAL_GPIO
+/*  Keil::Device:STM32Cube HAL:PWR:1.2.7 */
+#define RTE_DEVICE_HAL_PWR
+/*  Keil::Device:STM32Cube HAL:RCC:1.2.7 */
+#define RTE_DEVICE_HAL_RCC
+/*  Keil::Device:STM32Cube HAL:RNG:1.2.7 */
+#define RTE_DEVICE_HAL_RNG
+/*  Keil::Device:Startup:1.2.4 */
+#define RTE_DEVICE_STARTUP_STM32F7XX    /* Device Startup for STM32F7 */
+
+
+#endif /* RTE_COMPONENTS_H */
