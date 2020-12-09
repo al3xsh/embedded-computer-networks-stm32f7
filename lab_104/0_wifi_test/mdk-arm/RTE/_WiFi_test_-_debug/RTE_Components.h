596cc69250dc79a41d53ab3e9c7bf9b1ad078b2c
Alex Shenfield <a.shenfield@shu.ac.uk>
2020-12-09 12:20:34 +0000

    added some WiFi test code

---
+++ lab_104/0_wifi_test/mdk-arm/RTE/_WiFi_test_-_debug/RTE_Components.h
@@ -0,0 +1,56 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'WiFi Test' 
+ * Target:  'WiFi test - debug' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f7xx.h"
+
+/*  ARM::CMSIS:RTOS2:Keil RTX5:Library:5.5.2 */
+#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
+        #define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
+/*  Keil.ARM Compiler::Compiler:I/O:STDOUT:ITM:1.2.0 */
+#define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
+          #define RTE_Compiler_IO_STDOUT_ITM      /* Compiler I/O: STDOUT ITM */
+/*  Keil::CMSIS Driver:USART:1.14.0 */
+#define RTE_Drivers_USART1              /* Driver USART1 */
+        #define RTE_Drivers_USART2              /* Driver USART2 */
+        #define RTE_Drivers_USART3              /* Driver USART3 */
+        #define RTE_Drivers_USART4              /* Driver USART4 */
+        #define RTE_Drivers_USART5              /* Driver USART5 */
+        #define RTE_Drivers_USART6              /* Driver USART6 */
+        #define RTE_Drivers_USART7              /* Driver USART7 */
+        #define RTE_Drivers_USART8              /* Driver USART8 */
+/*  Keil::CMSIS Driver:WiFi:ESP8266:UART:1.4.0 */
+#define RTE_Drivers_WiFi_ESP8266_UART   /* Driver WiFi ESP8266 (UART) */
+/*  Keil::Device:STM32Cube Framework:Classic:1.2.8 */
+#define RTE_DEVICE_FRAMEWORK_CLASSIC
+/*  Keil::Device:STM32Cube HAL:Common:1.2.8 */
+#define RTE_DEVICE_HAL_COMMON
+/*  Keil::Device:STM32Cube HAL:Cortex:1.2.8 */
+#define RTE_DEVICE_HAL_CORTEX
+/*  Keil::Device:STM32Cube HAL:DMA:1.2.8 */
+#define RTE_DEVICE_HAL_DMA
+/*  Keil::Device:STM32Cube HAL:GPIO:1.2.8 */
+#define RTE_DEVICE_HAL_GPIO
+/*  Keil::Device:STM32Cube HAL:PWR:1.2.8 */
+#define RTE_DEVICE_HAL_PWR
+/*  Keil::Device:STM32Cube HAL:RCC:1.2.8 */
+#define RTE_DEVICE_HAL_RCC
+/*  Keil::Device:STM32Cube HAL:RNG:1.2.8 */
+#define RTE_DEVICE_HAL_RNG
+/*  Keil::Device:Startup:1.2.5 */
+#define RTE_DEVICE_STARTUP_STM32F7XX    /* Device Startup for STM32F7 */
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ lab_104/0_wifi_test/src/socket_startup.c
@@ -0,0 +1,95 @@
+/*
+ * socket_startup.c
+ *
+ * initialise the wifi module and connect to the specified access point
+ * using the supplied credentials
+ *
+ * author:    Dr. Alex Shenfield
+ * date:      17/11/2020
+ * purpose:   55-604481 embedded computer networks - lab 104
+ */
+
+// include functions from the standard c libraries
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+
+// include the ARM CMSIS-Driver WiFi driver
+#include "Driver_WiFi.h"
+
+// include the header file with the WiFi credentials in
+#include "my_credentials.h"
+
+// the wifi driver is defined and created elsewhere, but we'll use it here
+extern ARM_DRIVER_WIFI Driver_WiFi0;
+
+// WIFI INITIALISATION UP CODE
+
+
+int32_t scan_wifi(void)
+{
+  ARM_WIFI_SCAN_INFO_t scan_info[8];
+ 
+  // print some status messages ...
+  printf("scanning for WiFi networks ...\n");
+  
+  // initialise the wifi driver and scan for available networks
+  Driver_WiFi0.Initialize(NULL);
+  Driver_WiFi0.PowerControl(ARM_POWER_FULL);
+  int num = Driver_WiFi0.Scan(scan_info, 8U);   
+  printf("found %d WiFi networks\n", num);
+  
+  // print details of available WiFi networks
+  for(int i = 0; i < num; i++) 
+  {
+    printf("%d. ssid=%s, rssi = %d\n", i, scan_info[i].ssid, scan_info[i].rssi);
+  }
+  return 0;
+}
+
+
+// start up the wifi connection
+int32_t socket_startup(void)
+{
+  // wifi configuration structure
+  ARM_WIFI_CONFIG_t wifi_config;
+
+  // print a status message
+  printf("connecting to WiFi ...\r\n");
+
+  // initialise the wifi driver
+  Driver_WiFi0.Initialize(NULL);
+  Driver_WiFi0.PowerControl(ARM_POWER_FULL);
+
+  // zero the wifi configuration structure and set it up with the supplied
+  // values
+  memset((void *)&wifi_config, 0, sizeof(wifi_config));
+  wifi_config.ssid     = SSID;
+  wifi_config.pass     = PASSWORD;
+  wifi_config.security = SECURITY_TYPE;
+  wifi_config.ch       = 0U;
+
+  // activate the wifi driver and connect to the specified access point
+  // using the supplied credentials
+  Driver_WiFi0.Activate(0U, &wifi_config);
+
+  // check whether we connected ok and display an appropriate status message
+  if(Driver_WiFi0.IsConnected() == 0U)
+  {
+    printf("WiFi network connection failed!\r\n");
+    return (-1);
+  }
+  else
+  {
+    printf("WiFi network connection succeeded!\r\n");
+  }
+
+  // get the ip address assigned to the embedded board via dhcp and print
+  // it as a status message
+  uint8_t ip[4];
+  Driver_WiFi0.GetOption(0U, ARM_WIFI_IP, &ip, (uint32_t *)sizeof(ip));
+  printf("IP = %d.%d.%d.%d\r\n", ip[0], ip[1], ip[2], ip[3]);
+
+  // return success
+  return 0;
+}
