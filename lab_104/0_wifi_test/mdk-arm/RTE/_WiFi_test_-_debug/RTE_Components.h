e7883c62745847d72cd77321819cd10d73d98e15
Alex Shenfield <alex.shenfield@gmail.com>
2021-11-25 15:30:26 +0000

    updates after code testing

--- lab_104/0_wifi_test/mdk-arm/RTE/_WiFi_test_-_debug/RTE_Components.h
+++ lab_104/0_wifi_test/mdk-arm/RTE/_WiFi_test_-_debug/RTE_Components.h
@@ -33,29 +33,29 @@
         #define RTE_Drivers_USART8              /* Driver USART8 */
 /* Keil::CMSIS Driver:WiFi:ESP8266:UART:1.4.0 */
 #define RTE_Drivers_WiFi_ESP8266_UART   /* Driver WiFi ESP8266 (UART) */
-/* Keil::Device:STM32Cube Framework:Classic:1.2.9 */
+/* Keil::Device:STM32Cube Framework:Classic:1.2.8 */
 #define RTE_DEVICE_FRAMEWORK_CLASSIC
-/* Keil::Device:STM32Cube HAL:Common:1.2.9 */
+/* Keil::Device:STM32Cube HAL:Common:1.2.8 */
 #define RTE_DEVICE_HAL_COMMON
-/* Keil::Device:STM32Cube HAL:Cortex:1.2.9 */
+/* Keil::Device:STM32Cube HAL:Cortex:1.2.8 */
 #define RTE_DEVICE_HAL_CORTEX
-/* Keil::Device:STM32Cube HAL:DMA:1.2.9 */
+/* Keil::Device:STM32Cube HAL:DMA:1.2.8 */
 #define RTE_DEVICE_HAL_DMA
-/* Keil::Device:STM32Cube HAL:GPIO:1.2.9 */
+/* Keil::Device:STM32Cube HAL:GPIO:1.2.8 */
 #define RTE_DEVICE_HAL_GPIO
-/* Keil::Device:STM32Cube HAL:PWR:1.2.9 */
+/* Keil::Device:STM32Cube HAL:PWR:1.2.8 */
 #define RTE_DEVICE_HAL_PWR
-/* Keil::Device:STM32Cube HAL:RCC:1.2.9 */
+/* Keil::Device:STM32Cube HAL:RCC:1.2.8 */
 #define RTE_DEVICE_HAL_RCC
-/* Keil::Device:STM32Cube HAL:RNG:1.2.9 */
+/* Keil::Device:STM32Cube HAL:RNG:1.2.8 */
 #define RTE_DEVICE_HAL_RNG
-/* Keil::Device:STM32Cube LL:Common:1.2.9 */
+/* Keil::Device:STM32Cube LL:Common:1.2.8 */
 #define RTE_DEVICE_LL_COMMON
-/* Keil::Device:STM32Cube LL:PWR:1.2.9 */
+/* Keil::Device:STM32Cube LL:PWR:1.2.8 */
 #define RTE_DEVICE_LL_PWR
-/* Keil::Device:STM32Cube LL:RCC:1.2.9 */
+/* Keil::Device:STM32Cube LL:RCC:1.2.8 */
 #define RTE_DEVICE_LL_RCC
-/* Keil::Device:STM32Cube LL:UTILS:1.2.9 */
+/* Keil::Device:STM32Cube LL:UTILS:1.2.8 */
 #define RTE_DEVICE_LL_UTILS
 /* Keil::Device:Startup:1.2.5 */
 #define RTE_DEVICE_STARTUP_STM32F7XX    /* Device Startup for STM32F7 */
--- lab_104/1_mqtt_messages/mdk-arm/RTE/_MQTT_Message_Test_-_Debug/Pre_Include_Global.h
+++ lab_104/1_mqtt_messages/mdk-arm/RTE/_MQTT_Message_Test_-_Debug/Pre_Include_Global.h
@@ -10,9 +10,9 @@
 #ifndef PRE_INCLUDE_GLOBAL_H
 #define PRE_INCLUDE_GLOBAL_H
 
-/* Keil::Device:STM32Cube HAL:Common:1.2.9 */
+/* Keil::Device:STM32Cube HAL:Common:1.2.8 */
 #define USE_HAL_DRIVER
-/* Keil::Device:STM32Cube LL:Common:1.2.9 */
+/* Keil::Device:STM32Cube LL:Common:1.2.8 */
 #define USE_FULL_LL_DRIVER
 
 
